2953af1174175bdb56688747354226e9a3e02a56
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-04-15 15:01:45 +0545

    First Commit, pushing navbar code

---
+++ front_end/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <h1 className="bg-red-200">HELlo There</h1>
+      <br></br>
+      HIHIHI
+    </div>
+  );
+};
+
+export default App;
