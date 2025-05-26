8a3c626b51c6cefdef3cc26524a0ae8e348503aa
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-05-26 21:46:18 +0545

    Commit 6 fro deploying

--- back_end/index.js
+++ back_end/index.js
@@ -29,25 +29,11 @@ configDotenv()
 const app=express()
 console.clear()
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://nava-tara-project-mern.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
-  
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // respond to preflight
-  }
-
-  next();
-});
-
-
 //middlewares
 app.use(express.json())
 app.use(cors(
     {
-        origin:["https://nava-tara-project-mern.vercel.app"],
+        origin:["http://localhost:5173"],
         credentials:true
     }
 ))
